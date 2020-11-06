0c5f51c57216604f7a0d8105fb18f314a91d9101
AndreasOeS <mail.andreas.s@gmail.com>
2020-11-06 16:49:40 +0100

    Startet på at hente ingrediencer fra database

--- app/src/main/java/com/example/myeatup/ui/AddIngredient.java
+++ app/src/main/java/com/example/myeatup/ui/AddIngredient.java
@@ -3,10 +3,16 @@ package com.example.myeatup.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.GridView;
 
 import com.example.myeatup.R;
 import com.example.myeatup.firebasedata.IngredientDTO;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -14,6 +20,7 @@ public class AddIngredient extends AppCompatActivity {
 
     GridView dialogGridview;
     GridviewAdapter gvAdapter;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +36,53 @@ public class AddIngredient extends AppCompatActivity {
 
 
                 dialogGridview.setAdapter(gvAdapter);
+                gvAdapter.notifyDataSetChanged();
     }
 
     private ArrayList<IngredientDTO> getIngredientListFromDataBase() {
-        ArrayList<IngredientDTO> listToReturn = new ArrayList<>();
-        listToReturn.add(new IngredientDTO("0","Appel"));
-        listToReturn.add(new IngredientDTO("1","Orange"));
-        listToReturn.add(new IngredientDTO("2","Pineappel"));
+
+        final ArrayList<IngredientDTO> listToReturn = new ArrayList<>();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("ingredients");
+
+
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int i = 0;
+
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()){
+
+                    IngredientDTO ingre = postSnapshot.getValue(IngredientDTO.class);
+                    String id = ingre.getID();
+                    String name = ingre.getName();
+                    listToReturn.add(new IngredientDTO(id, name));
+                    System.out.println(listToReturn.get(i).getName() + i + "inder");
+                    i++;
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        mDatabase.addListenerForSingleValueEvent(postListener);
+
+
+
+        //listToReturn.add(new IngredientDTO("0","Appel"));
+        //listToReturn.add(new IngredientDTO("1","Orange"));
+        //listToReturn.add(new IngredientDTO("2","Pineappel"));
+        //for(int i = 0; i < listToReturn.size();i++){
+        //    System.out.println(listToReturn.get(i).getName() + i + "ydre");
+
+       // }
+
+
+
 
         return listToReturn;
     }
