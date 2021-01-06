ca70ddefe42a38c61b2d26adfb78b1cca7a0fed1
ak5ina <43174060+ak5ina@users.noreply.github.com>
2021-01-06 14:45:34 +0100

    test of edittest

--- app/src/main/java/com/scrippy2/myeatup/ui/AddIngredient.java
+++ app/src/main/java/com/scrippy2/myeatup/ui/AddIngredient.java
@@ -3,17 +3,15 @@ package com.scrippy2.myeatup.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.GridView;
 
 import com.scrippy2.myeatup.MainActivity;
 import com.scrippy2.myeatup.R;
 import com.scrippy2.myeatup.firebasedata.IngredientDTO;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -35,34 +33,61 @@ public class AddIngredient extends AppCompatActivity {
 
         final ArrayList<IngredientDTO> listToReturn = new ArrayList<>();
 
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("ingredients");
+
+
+
 
         //OPEN GRIDVIEW
+
+        StartGridView();
+
+        edittext.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length() != 0)
+                    try {
+                        UpdateGridView();
+                    } catch (Exception e){
+                        System.out.println("FAIL");
+                        System.out.println(e);
+                    }
+                else {
+                    System.out.println("FUCK");
+                }
+            }
+        });
+
+    }
+
+    private void StartGridView(){
         dialogGridview = (GridView) findViewById(R.id.gridview);
         gvAdapter = new GridviewAdapter(this,R.layout.gridview_single_object, MainActivity.INGREDIENTLIST);
         dialogGridview.setAdapter(gvAdapter);
+    }
+
+    private void UpdateGridView(){
 
 
-        //OLD WAYS OF GETTING THE DATA FROM THE DATABASE
-//        ValueEventListener postListener = new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//
-//                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()){
-//
-//                    IngredientDTO ingre = postSnapshot.getValue(IngredientDTO.class);
-//                    String id = ingre.getID();
-//                    String name = ingre.getName();
-//                    listToReturn.add(new IngredientDTO(id, name));
-//                }
-//                gvAdapter.notifyDataSetChanged();
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        };
-//        mDatabase.addListenerForSingleValueEvent(postListener);
+        gvAdapter.clear();
+
+        for (IngredientDTO ing : MainActivity.INGREDIENTLIST){
+            if (ing.getName().toLowerCase().startsWith(edittext.getText().toString().toLowerCase())){
+                gvAdapter.add(ing);
+            }
+        }
+
+        gvAdapter.notifyDataSetChanged();
+        System.out.println("HEJ");
+
     }
 }
